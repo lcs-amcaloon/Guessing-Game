784567f7ca473e82aa234c018022eb1aa1d01396
Angus McAloon <AMcAloon@lcs.on.ca>
2020-11-17 14:39:01 -0500

    Initial Commit

---
+++ Guessing Game/Guessing_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guessing_GameApp.swift
+//  Guessing Game
+//
+//  Created by Angus McAloon on 2020-11-17.
+//
+
+import SwiftUI
+
+@main
+struct Guessing_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
