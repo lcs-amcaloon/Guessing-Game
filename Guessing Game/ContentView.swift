8c16e5ef706b03fb89d5af6bda95553e86494d73
Angus McAloon <AMcAloon@lcs.on.ca>
2020-11-17 15:03:34 -0500

    made user interface, added button and user input

--- Guessing Game/ContentView.swift
+++ Guessing Game/ContentView.swift
@@ -8,10 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var userGuess = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section{
+                Text("I'm thinking of a number between 1 and 100.")
+                Text("Guess What it is!")
+                }
+                
+                Section{
+                    TextField("Enter Guess", text: $userGuess)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section{
+                    Button("Submit Guess:"){
+                        //what we want the button to do
+                    }
+                    
+                    Text("You Guessed \(userGuess)")
+                }
+            }
+            .navigationBarTitle("LunchTime")
+        }
+
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
